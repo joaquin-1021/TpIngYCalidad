26706b43a0d60934dfa0354e4a94fcf92bff23ee
joaquin-1021 <joaqinn.1021@gmail.com>
2025-09-23 21:59:51 -0300

    inciso i modificacion A

--- src/main/java/fr/codecake/spotifyclone/catalogcontext/application/vo/SongTitleVO.java
+++ src/main/java/fr/codecake/spotifyclone/catalogcontext/application/vo/SongTitleVO.java
@@ -3,4 +3,9 @@ package fr.codecake.spotifyclone.catalogcontext.application.vo;
 import jakarta.validation.constraints.NotBlank;
 
 public record SongTitleVO(@NotBlank String value) {
+
+
+
+
+    
 }
